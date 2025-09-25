49ec31977939cd0980d609b51dbc7789618dbc3d
Atiqurrozi <atiqrozi14@gmail.com>
2025-09-25 22:25:11 +0700

    fix bug

--- src/config/db.js
+++ src/config/db.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
         serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
         socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
       };
-      
+
       await mongoose.connect(process.env.MONGODB_URI, options);
       console.log("MongoDB Connected");
     }
